8b270a8f7b4315934af55a7e91cfa3649e3713a4
Sergey Pertsev <s.pertsev@outlook.com>
2022-03-28 12:39:11 +0300

    Changed locators

--- custom_libraries/CustomKeywords.py
+++ custom_libraries/CustomKeywords.py
@@ -8,11 +8,11 @@ def verify_city_name_after_change(entered_city_name):
     driver.get("https://weather-app-madza.netlify.app")
     driver.implicitly_wait(20)
     time.sleep(3)
-    city_input_bar = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[2]/div[1]/input')
+    city_input_bar = driver.find_element(By.XPATH, '//input[@placeholder='Search a city...']')
     city_input_bar.send_keys(entered_city_name)
     city_input_bar.send_keys(Keys.ENTER)
     time.sleep(3)
-    city_name_element = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/h1[1]')
+    city_name_element = driver.find_element(By.XPATH, '//h1[contains(@class,'MainCard_location__Zmash')]')
     if not city_name_element.text.__contains__(entered_city_name):
         raise Exception('City name on the page differs from the entered one')
     time.sleep(1)
